f3b16b077845743efe5df1fc2a6b4804d8cdcce3
Desmond Leung <dleungtm@gmail.com>
2018-02-05 16:55:47 -0800

    update database configuration and restore permissions for app_user

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 module.exports = {
   'connection': {
     'host': 'localhost',
-    'user': 'root',
-    'password': ''
+    'user': 'app_user',
+    'password': 'cc_ecs_winter_t2'
   },
   'database': 'cc_expense_claims_system',
 };
