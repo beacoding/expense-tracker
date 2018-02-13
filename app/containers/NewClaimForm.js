df40523c5e47cb7316ecae3f7cbd5f0933075db1
Duncan <duncantruong@gmail.com>
2018-02-13 01:30:01 -0800

    refactored new claim form code, changed appearance, and added form validation

--- app/containers/NewClaimForm.js
+++ app/containers/NewClaimForm.js
@@ -15,60 +15,116 @@ class NewClaimForm extends React.Component {
 
   }
 
+  renderField(field) {
+    const {meta: {touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
 
+    return (
+      <div className = {className}>
+        <label>{field.label}</label>
+        {/* the ... gets us everything associated with field.input such as onChange, onFocus, etc.*/}
+        <input className="form-control" placeholder={field.placeholder} type={field.type} {...field.input} />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  renderTextAreaField(field) {
+    const {meta: {touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
+
+    return (
+      <div className = {className}>
+        <label>{field.label}</label>
+        {/* the ... gets us everything associated with field.input such as onChange, onFocus, etc.*/}
+        <textarea className="form-control" {...field.input} />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  renderCompanyDropdownField(field) {
+    const {meta: {touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
+
+    return (
+      <div className = {className}>
+        <label>{field.label}</label>
+        {/* the ... gets us everything associated with field.input such as onChange, onFocus, etc.*/}
+        <select className="form-control" {...field.input}>
+          <option value="" disabled> Select a company </option>
+          <option value={1}>CCFM- Financial Management</option>
+          <option value={2}>CCSCU- Banking</option>
+          <option value={3}>TFL-Travelers Finance LTD</option>
+        </select>
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  renderCostCenterDropdownField(field) {
+    const {meta: {touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
+
+    return (
+      <div className = {className}>
+        <label>{field.label}</label>
+        {/* the ... gets us everything associated with field.input such as onChange, onFocus, etc.*/}
+        <select className="form-control" {...field.input}>
+          <option value="" disabled> Select a cost center </option>
+          <option value={500}>500</option>
+          <option value={501}>501</option>
+          <option value={600}>600</option>
+        </select>
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
 //form for submit new claim initial items
   render () {
     const { handleSubmit, pristine, reset, submitting } = this.props;
     return (
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Coast Account Number</label>
-          <div>
             <Field
+              label="14-digit Coast Capital Account Number"
+              // name = what piece of state this field is going to produce
               name="ccaccountnumber"
-              component="input"
+              component={this.renderField}
               type="number"
               placeholder="14 digit Coast Capital account"
             />
-          </div>
-        </div>
-        <div>
-          <label htmlFor="nocoastacc">No Coast Capital Account</label>
-          <div>
+          {/*what is htmlFor for?*/}
+          {/*<label htmlFor="nocoastacc">No Coast Capital Account</label>*/}
             <Field
+              label="No Coast Capital Account"
               name="payroll"
               id="payroll"
               default={false}
-              component="input"
+              component={this.renderField}
               type="checkbox"
             />
-          </div>
-        </div>
-        <div>
-          <label>Description</label>
-          <div>
             <Field
+              label="Description"
               name="description"
-              component="input"
+              component={this.renderField}
               type="text"
               placeholder="Lunch"
             />
-          </div>
-        </div>
-        <div>
-          <label>Company</label>
-          <div>
-          <Field name="companyid" component="select">
-              <option />
-              <option value={1}>CCFM- Financial Management</option>
-              <option value={2}>CCSCU- Banking</option>
-              <option value={3}>TFL-Travelers Finance LTD</option>
+
+          <Field
+            label="Company"
+            name="companyid"
+            component={this.renderCompanyDropdownField}>
             </Field>
-          </div>
-        </div>
-        <div>
-          <label></label>
-          <div>
+
             {/* <label>
               <Field name="sex" component="input" type="radio" value="male" />
               {' '}
@@ -79,39 +135,70 @@ class NewClaimForm extends React.Component {
               {' '}
               Female
             </label> */}
-          </div>
-        </div>
-        <div>
-          <label>Cost Center</label>
-          <div>
-            <Field name="costcenter" component="select">
-              <option />
-              <option value={500}>500</option>
-              <option value={600}>600</option>
-              <option value={501}>501</option>
+            <Field
+              label="Cost Center"
+              name="costcenter"
+              component={this.renderCostCenterDropdownField}>
             </Field>
-          </div>
-        </div>
-       
-        <div>
-          <label>Notes</label>
-          <div>
-            <Field name="notes" component="textarea" />
-          </div>
-        </div>
-        <div>
-          <button type="submit" disabled={pristine || submitting}>Submit</button>
-          <button type="button" disabled={pristine || submitting} onClick={reset}>
+            <Field
+              label="Notes"
+              name="notes"
+              component={this.renderTextAreaField}
+              type="textarea"
+            />
+          <button type="submit" className="btn btn-primary" disabled={pristine || submitting}>Submit</button>
+          <button type="button" className="btn btn-danger" disabled={pristine || submitting} onClick={reset}>
             Clear Values
           </button>
-        </div>
       </form>
     );
    }
   }
 
+  function validate(values){
+    // create empty errors object to return
+
+    const errors = {};
+
+    // validate the inputs from 'values'
+
+      if (!/^(0|[1-9][0-9]{13})$/i.test(values.ccaccountnumber)){
+          errors.ccaccountnumber = "Invalid account number!";
+      }
+      // if (!values.payroll){
+      //     errors.payroll = "Please enter an account number or check this box!";
+      // }
+      if (!values.description){
+        console.log("description is talking" + values.notes);
+          errors.description = "Please enter a description!";
+      }
+      if (!values.companyid){
+          console.log(values);
+          console.log("companyid is talking" + values.costcenter);
+          errors.companyid = "Please select a company!";
+      }
+      if (!values.costcenter){
+        console.log(values);
+        console.log("costcenter is talking" + values.companyid);
+          errors.costcenter = "Please select a cost center! ";
+      }
+      if (!values.notes){
+        console.log(values);
+        console.log("notes is talking" + values.description);
+          errors.notes = "Please enter some notes!";
+      }
+
+
+      // if errors is empty, the form is fine to submit
+    // if errors has any properties, redux form assumes form is invalid
+
+    return errors;
+
+  }
+
   export default reduxForm({
-    form: 'NewClaimForm', // a unique identifier for this form
+      validate,
+    form: 'NewClaimForm' // a unique identifier for this form
   })(NewClaimForm);
   
   //   render() {  
