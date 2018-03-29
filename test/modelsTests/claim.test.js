ae638b47bfc372ab928a2d922a24e7fd6835e460
minnieliu <minnieliu96@hotmail.com>
2018-03-28 17:51:25 -0700

    todo

--- test/modelsTests/claim.test.js
+++ test/modelsTests/claim.test.js
@@ -3,18 +3,6 @@ var assert = require('chai').assert;
 var chaiHttp = require('chai-http');
 
 describe('Testing Claim functions', function(){
-    // it('should find all claims with the employee', function(){
-    //     var employee = {
-    //         id:"0",
-    //         is_active:"T",
-    //         email:"dwade@domain.ca",
-    //         first_name:"Dwayne",
-    //         last_name:"Wade",
-    //         password:"pw123"
-    //     }
-    //     var claims = claim.findAllWithEmployee(employee)
-    //     console.log("claims:" + claims);
-    // })
-
+    //TODO write tests for claim functions
 })
 
