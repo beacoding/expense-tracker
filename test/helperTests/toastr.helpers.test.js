82cd7258f9cf1a9e3a4fb6dd754f979915e84440
minnieliu <minnieliu96@hotmail.com>
2018-03-28 17:49:30 -0700

    tests

--- test/helperTests/toastr.helpers.test.js
+++ test/helperTests/toastr.helpers.test.js
@@ -7,12 +7,9 @@ var chaiHttp = require('chai-http');
 
 describe('Testing Toastr Helpers', function(){
     it('should match toastrOptions', function(){
-        const toastrOptions = {
-            timeOut: 0, // by setting to 0 it will prevent the auto close
-            showCloseButton: true// true by default
-        }
+
         const errorOptions = toastrHelpers.getErrorOptions()
-        assert.equal(errorOptions['timeOut'], 0)
+        assert.equal(errorOptions['timeOut'], 10000)
         assert.equal(errorOptions['showCloseButton'], true)
     })
 })
